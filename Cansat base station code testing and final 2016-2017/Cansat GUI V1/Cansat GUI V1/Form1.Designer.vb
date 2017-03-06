fba7207e833d8ad9f55ade3bacdc974825016954
Jai Davey <122077@student.eastkent.ac.uk>
2017-03-06 20:07:04 +0000

    VB gui code v1

---
+++ Cansat base station code testing and final 2016-2017/Cansat GUI V1/Cansat GUI V1/Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbPort = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnDisconnect = New System.Windows.Forms.Button()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.cmbBaud = New System.Windows.Forms.ComboBox()
+        Me.txtIn = New System.Windows.Forms.TextBox()
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmbPort
+        '
+        Me.cmbPort.FormattingEnabled = True
+        Me.cmbPort.Location = New System.Drawing.Point(6, 42)
+        Me.cmbPort.Name = "cmbPort"
+        Me.cmbPort.Size = New System.Drawing.Size(121, 24)
+        Me.cmbPort.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.btnDisconnect)
+        Me.GroupBox1.Controls.Add(Me.btnConnect)
+        Me.GroupBox1.Controls.Add(Me.cmbBaud)
+        Me.GroupBox1.Controls.Add(Me.cmbPort)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 35)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(447, 100)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Connection"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(163, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "baudrate"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "com"
+        '
+        'btnDisconnect
+        '
+        Me.btnDisconnect.Location = New System.Drawing.Point(341, 44)
+        Me.btnDisconnect.Name = "btnDisconnect"
+        Me.btnDisconnect.Size = New System.Drawing.Size(98, 23)
+        Me.btnDisconnect.TabIndex = 3
+        Me.btnDisconnect.Text = "Disconnect"
+        Me.btnDisconnect.UseVisualStyleBackColor = True
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(260, 43)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 2
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'cmbBaud
+        '
+        Me.cmbBaud.FormattingEnabled = True
+        Me.cmbBaud.Location = New System.Drawing.Point(133, 42)
+        Me.cmbBaud.Name = "cmbBaud"
+        Me.cmbBaud.Size = New System.Drawing.Size(121, 24)
+        Me.cmbBaud.TabIndex = 1
+        '
+        'txtIn
+        '
+        Me.txtIn.Location = New System.Drawing.Point(465, 35)
+        Me.txtIn.Multiline = True
+        Me.txtIn.Name = "txtIn"
+        Me.txtIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtIn.Size = New System.Drawing.Size(859, 100)
+        Me.txtIn.TabIndex = 2
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(465, 141)
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(859, 22)
+        Me.txtMessage.TabIndex = 3
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(384, 141)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 4
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(467, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "com"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(529, 15)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(34, 17)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "com"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(1105, 15)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 17)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "com"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(660, 15)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(34, 17)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "com"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(724, 15)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(34, 17)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "com"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(784, 15)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(34, 17)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "com"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(856, 15)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(34, 17)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "com"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(929, 15)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(34, 17)
+        Me.Label11.TabIndex = 14
+        Me.Label11.Text = "com"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(982, 15)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(34, 17)
+        Me.Label12.TabIndex = 15
+        Me.Label12.Text = "com"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(1045, 15)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(34, 17)
+        Me.Label13.TabIndex = 16
+        Me.Label13.Text = "com"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(594, 15)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(34, 17)
+        Me.Label14.TabIndex = 17
+        Me.Label14.Text = "com"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(1162, 15)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 17)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "com"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(1224, 15)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(14, 17)
+        Me.Label15.TabIndex = 19
+        Me.Label15.Text = "x"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1343, 599)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.txtIn)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "low risk GUI v1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbPort As ComboBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnDisconnect As Button
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents cmbBaud As ComboBox
+    Friend WithEvents txtIn As TextBox
+    Friend WithEvents txtMessage As TextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label15 As Label
+End Class
---
+++ Cansat base station code testing and final 2016-2017/Cansat GUI V1/Cansat GUI V1/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.IO.Ports
+Public Class Form1
+    Dim WithEvents sp As New SerialPort
+
+    Private Sub GetSerialPortNames()
+        For Each sport As String In My.Computer.Ports.SerialPortNames
+            cmbPort.Items.Add(sport)
+        Next
+    End Sub
+
+    Sub ShowString(ByVal myString As String)
+        txtIn.AppendText(myString)
+    End Sub
+
+    Delegate Sub myMethodDelegate(ByVal [text] As String)
+    Dim myDelegate As New myMethodDelegate(AddressOf ShowString)
+
+    Private Sub Form1_Load(sender As System.Object, e As EventArgs) Handles MyBase.Load
+        Dim BaudRates() As String = {"300", "1200", "2400", "4800", "9600", "14400", "19200", "28800", "38400", "57600", "115200"}
+        cmbBaud.Items.AddRange(BaudRates)
+        cmbBaud.SelectedIndex = 4
+        Try
+            GetSerialPortNames()
+            cmbPort.SelectedIndex = 0
+        Catch
+            MsgBox("No ports connected.")
+        End Try
+    End Sub
+
+    Private Function BaudRates() As Object
+        Throw New NotImplementedException()
+    End Function
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        Try
+            sp.BaudRate = cmbBaud.SelectedItem.ToString
+            sp.PortName = cmbPort.SelectedItem.ToString
+            sp.Open()
+            If sp.IsOpen Then
+                btnConnect.Visible = False
+                cmbPort.Enabled = False
+                cmbBaud.Enabled = False
+                btnDisconnect.Visible = True
+            End If
+        Catch
+            sp.Close()
+        End Try
+    End Sub
+
+    Private Sub btnDisconnect_Click(sender As Object, e As EventArgs) Handles btnDisconnect.Click
+        Try
+            sp.Close()
+            btnConnect.Visible = True
+            btnDisconnect.Visible = False
+            cmbPort.Enabled = True
+            cmbBaud.Enabled = True
+            Exit Sub
+        Catch
+            MessageBox.Show("Some kind of problem.")
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If sp.IsOpen() Then
+            MessageBox.Show("Disconnect before closing")
+            e.Cancel = True
+        End If
+    End Sub
+    Private Sub SerialPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles sp.DataReceived
+        Dim str As String = sp.ReadExisting()
+        Invoke(myDelegate, str)
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        If sp.IsOpen() Then
+            sp.WriteLine(txtMessage.Text)
+            txtMessage.Text = ""
+        End If
+    End Sub
+
+End Class
